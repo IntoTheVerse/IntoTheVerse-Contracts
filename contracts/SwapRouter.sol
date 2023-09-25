4cfe6d0073a21f5a8fd4461ff8ff53237b9b522a
Hicham Benhima <hichambenhima@hichams-macbook-pro.home>
2023-09-25 09:29:30 +0200

    fee tier for v3

--- contracts/SwapRouter.sol
+++ contracts/SwapRouter.sol
@@ -2,16 +2,47 @@
 pragma solidity >=0.8.19;
 
 import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
+import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Factory.sol";
+import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol";
 import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract SwapRouter {
     ISwapRouter public immutable uniswapRouter;
     IUniswapV2Router02 public immutable ubeswapRouter;
+    IUniswapV3Factory public immutable uniswapFactory;
 
-    constructor(address _uniswapRouter, address _ubeswapRouter) {
+    uint24[3] public feeTiers = [500, 3000, 10000]; // 0.05%, 0.3%, and 1% respectively
+
+    constructor(
+        address _uniswapRouter,
+        address _ubeswapRouter,
+        address _uniswapFactory
+    ) {
         uniswapRouter = ISwapRouter(_uniswapRouter);
         ubeswapRouter = IUniswapV2Router02(_ubeswapRouter);
+        uniswapFactory = IUniswapV3Factory(_uniswapFactory);
+    }
+
+    function calculateUniswapV3FeeTier(
+        address tokenA,
+        address tokenB
+    ) public view returns (uint24) {
+        for (uint i = 0; i < feeTiers.length; i++) {
+            address poolAddress = uniswapFactory.getPool(
+                tokenA,
+                tokenB,
+                feeTiers[i]
+            );
+            if (poolAddress != address(0)) {
+                // Pool exists for this fee tier
+                IUniswapV3Pool pool = IUniswapV3Pool(poolAddress);
+                uint24 poolFee = pool.fee();
+
+                return poolFee;
+            }
+        }
+        revert("No pool exists for the given token pair");
     }
 
     function swapOnV2(
@@ -46,8 +77,7 @@ contract SwapRouter {
         address tokenIn,
         address tokenOut,
         uint256 amountIn,
-        uint256 amountOutMin,
-        uint24 fee
+        uint256 amountOutMin
     ) external {
         require(
             IERC20(tokenIn).transferFrom(msg.sender, address(this), amountIn),
@@ -58,12 +88,13 @@ contract SwapRouter {
             "Approval failed"
         );
 
+        uint24 feeTiers = calculateUniswapV3FeeTier(tokenIn, tokenOut);
         // Define the params for the V3 swap
         ISwapRouter.ExactInputSingleParams memory params = ISwapRouter
             .ExactInputSingleParams({
                 tokenIn: tokenIn,
                 tokenOut: tokenOut,
-                fee: fee,
+                fee: feeTiers,
                 recipient: msg.sender,
                 deadline: block.timestamp + 300,
                 amountIn: amountIn,
